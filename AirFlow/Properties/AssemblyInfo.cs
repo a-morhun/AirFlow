ee68bbaf604758001a41b2b323431d2adc79701a
Artem <artem.morhun@valtech.com>
2018-04-30 18:28:09 +0300

    Add log4net into assembly properties

--- AirFlow/Properties/AssemblyInfo.cs
+++ AirFlow/Properties/AssemblyInfo.cs
@@ -35,3 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("AirFlow.Tests")]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]

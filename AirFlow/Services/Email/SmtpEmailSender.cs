aae44bb866ba18b51bf41f9d1fb13613d29d4ea8
Artem <artem.morhun@valtech.com>
2018-05-16 12:46:31 +0300

    Remove sensitive data from web.config

--- AirFlow/Services/Email/SmtpEmailSender.cs
+++ AirFlow/Services/Email/SmtpEmailSender.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AirFlow.Utilities;
+using System;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Configuration;
 using System.Net.Mail;
 
@@ -11,28 +13,53 @@ namespace AirFlow.Services.Email
     {
         private readonly SmtpClient _client;
         private readonly SmtpSection _section;
+        private readonly IAirFlowLogger _logger = new AirFlowLogger(typeof(SmtpEmailSender));
 
         public SmtpEmailSender()
         {
+            _logger.Debug($"Init sender client");
             _client = new SmtpClient();
             _section = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+           
             InitSmtpEnvironment();
+            SetClientCredentials();
         }
 
         private void InitSmtpEnvironment()
         {
+            _logger.Debug($"Initialized with ${_section.DeliveryMethod} delivery method");
             if (_section.DeliveryMethod == SmtpDeliveryMethod.SpecifiedPickupDirectory)
             {
                 Directory.CreateDirectory(_section.SpecifiedPickupDirectory.PickupDirectoryLocation);
             }
         }
 
+        private void SetClientCredentials()
+        {
+            if (_section.DeliveryMethod == SmtpDeliveryMethod.Network)
+            {
+                string username = ConfigurationManager.AppSettings["SendGridUserName"];
+                string password = ConfigurationManager.AppSettings["SendGridPassword"];
+
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    _logger.Error($"Credentials are missing for the client.");
+                    throw new InvalidOperationException("Credentials are missing for the client.");
+                }
+
+                _client.Credentials = new NetworkCredential(username, password);
+                _logger.Debug("Credentials for the client are read and applied");
+            }
+        }
+
         public void Send(EmailMessageType type, EmailMessageOptions options)
         {
             MailMessage message = EmailMessageFactory.Construct(type, options);
+            _logger.Debug($"Message of type {options.GetType()} was constructed");
 
             try
             {
+                _logger.Debug($"Message of type {options.GetType()} was sent");
                 _client.Send(message);
             }
             catch (Exception e)
